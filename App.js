1c846567192b2623a091baadd733ca2e22751480
Abhishek k Gupta <kabhishek3613@gmail.com>
2026-03-22 18:29:07 +0530

    Dynamic ActivityIndicator Button App

--- App.js
+++ App.js
@@ -1,11 +1,72 @@
-/* MAke Dynamic Radio Button
-   1. Make an Array Of Skills.
-   2. Apply Map Ovre the radio button
-   3. Update State With skill
-*/
- import React from "react";
-import ActivityIndicatorr from "./ActivityIndicatorr";
+// -- Topic ActivityIndicator (Dynamic Indicator) --
+// Make an Array of the Skills.
+// Apply Map Over The Radio Button
+// Update State With Skills
+ 
+import React, { useState } from "react";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
-export default function App() {
-  return <ActivityIndicatorr />;
-}
+const App = () => {
+  const [selectedRadio, setSelectedRadio] = useState(1);
+
+  return (
+    <View style={styles.main}>
+      
+      <TouchableOpacity onPress={() => setSelectedRadio(1)}>
+        <View style={styles.radioWrapper}>
+          <View style={styles.radio}>
+            {selectedRadio === 1 && <View style={styles.radioBg} />}
+          </View>
+          <Text style={styles.radioText}>Radio 1</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => setSelectedRadio(2)}>
+        <View style={styles.radioWrapper}>
+          <View style={styles.radio}>
+            {selectedRadio === 2 && <View style={styles.radioBg} />}
+          </View>
+          <Text style={styles.radioText}>Radio 2</Text>
+        </View>
+      </TouchableOpacity>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  radioText: {
+    fontSize: 20,
+    color: 'skyblue'
+  },
+
+  radio: {
+    height: 40,
+    width: 40,
+    borderColor: 'orange',
+    borderWidth: 2,
+    borderRadius: 20,
+    margin: 10
+  },
+
+  radioBg: {
+    backgroundColor: 'yellow',
+    height: 28,
+    width: 28,
+    borderRadius: 20,
+    margin: 4
+  },
+
+  radioWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
+
+});
+export default App;
