5faaffb021a0f0a175d51b06c20393087cf4c824
Abhishek k Gupta <kabhishek3613@gmail.com>
2026-03-26 17:13:13 +0530

    Pressable button in Native

--- App.js
+++ App.js
@@ -1,42 +1,31 @@
-// --- Topic - is Modal In Native 
-// Modal is a component is That Dispaly Content Top of Your Screen Box orr Dialog box
-// So Commonly Used For .Alert, .Forms, Conformation etc
+// --- Topic - Pressable in Native
+//  Pressable is a component In Native That Detect Interation Of Touch and Then Go to Conformation of Your UI Elements.
+//  Touch like (Press long, Press In, Press Out, and Normal Tab We can Use 4 Types Press)
 
-import React, { useState } from "react";
-import { View, Text, Button, Modal, StyleSheet } from "react-native";
+import React from "react";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
-
   return (
-    // Here on Press The Showing Modal Box 
     <View style={styles.container}>
-      <Button title="Open Modal" onPress={() => setVisible(true)} />
-
-      <Modal visible={visible} transparent={true} animationType="slide">
-        <View style={styles.modalView}>
-          <Text>This is a Modal</Text>
-
-          // --- Here Close Button when Information Getting The Click Close ---
-          <Button title="Close" onPress={() => setVisible(false)} />
-        </View>
-      </Modal>
+      <Pressable onPress={() => alert("Button Pressed!")}> // Here Simple Press (We can using Warn, alert etc)
+        <Text style={styles.text}>Click Me</Text>         // It is Button Showing on Your Screen --
+      </Pressable>
     </View>
   );
 };
 
-//  --- And Here Styling ---
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
-  modalView: {
-    flex: 1,
-    backgroundColor: "rgba(0,0,0,0.5)",
-    justifyContent: "center",
-    alignItems: "center",
+  text: {
+    padding: 10,
+    backgroundColor: "blue",
+    color: "white",
   },
 });
+
 export default App;
