013608692fc66d354a5de0c942aea6941ed1e6f8
Abhishek k Gupta <kabhishek3613@gmail.com>
2026-03-28 10:42:33 +0530

    Status Bar in React-Native

--- App.js
+++ App.js
@@ -3,14 +3,19 @@
 //  Touch like (Press long, Press In, Press Out, and Normal Tab We can Use 4 Types Press)
 
 import React from "react";
-import { View, Text, Pressable, StyleSheet } from "react-native";
+import { View, StatusBar, StyleSheet, Button } from "react-native";
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Pressable onPress={() => alert("Button Pressed!")}> // Here Simple Press (We can using Warn, alert etc)
-        <Text style={styles.text}>Click Me</Text>         // It is Button Showing on Your Screen --
-      </Pressable>
+      <StatusBar backgroundColor="orange" 
+      barStyle="light-content"  
+      hidden = {false}
+      />
+
+      <Button title = 'Toogle Status Bar' onPress={() => setInstanceHandle(!Hide)} />
+        
+      <Button title = 'Toogle Status Bar' onPress={() => setBarStyle("dark-content")}/>
     </View>
   );
 };
@@ -19,12 +24,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    padding: 10,
-    backgroundColor: "blue",
-    color: "white",
   },
 });
 
