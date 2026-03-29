90e5c041e9a20bf01ff9a24c0d93a77089658717
Abhishek k Gupta <kabhishek3613@gmail.com>
2026-03-29 10:30:51 +0530

    Modal 1.2 Modal Pop-up

--- App.js
+++ App.js
@@ -1,32 +1,52 @@
-// --- Topic - Platform 
-// What is Platform  -> Platform Using For Detect Which OS(App Running) on Your PC (Android or iOS).
+// --- Topic - Modal Pop-up
+// Like Showing Button to Clicking And pop some Same Changes on Your Screen.
 
+import React, { useState } from "react";
+import {View, StyleSheet, Button, Text, Modal} from 'react-native';
 
-import React from "react";
-import { View, StyleSheet, Button, Platform, Text } from "react-native";
+const App =() =>{
+  const [Show, setShow] =useState(false)
 
-const App = () => {
-  const textColor = Platform.select({ // Here Checking PlatFrom.select
-    android: "green",
-    ios: "blue",
-    size: 100,
-  });
-
-  return (
-    <View style={styles.container}>
-      <Text style={{ color: textColor}}>
-        Hello Platform!
-      </Text>
-    </View>
-  );
+return(
+  <View style = {styles.container}>
+    {
+      Show?
+      <View style = {styles.Modal}>
+      <View style = {styles.body}>
+      <Text> Some Text</Text>
+      <Button title='Close' onPress={() => setShow(false)} />
+        </View>
+        </View>
+        :null
+    }
+    <Button title='Open Dialog' onPress={() => setShow(true)} />
+        </View>
+);
 };
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: "flex-end",
   },
-});
-
+  Modal:{
+    flex: 1,
+    backgroundColor: 'rgba(152, 158, 154, 0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
