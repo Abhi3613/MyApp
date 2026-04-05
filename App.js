70b85670eb470b61d2b5af585ab7ca8863830145
Abhishek k Gupta <kabhishek3613@gmail.com>
2026-04-05 17:26:12 +0530

    FlatList With API

--- App.js
+++ App.js
@@ -1,42 +1,72 @@
-// --- Topic - Modal Pop-up
-// Like Showing Button to Clicking And pop some Same Changes on Your Screen.
+ import React, { useEffect, useState } from "react";
+import { Text, View, FlatList, ActivityIndicator, StyleSheet } from "react-native";
 
-import React, { useState } from "react";
-import {View, StyleSheet, Button, Text, Modal} from 'react-native';
+const App = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-const App =() =>{
-  const [Show, setShow] =useState(false)
-
-return(
-  <View style = {styles.container}>
-    {
-      Show?
-      <View style = {styles.Modal}>
-      <View style = {styles.body}>
-      <Text> Some Text</Text>
-      <Button title='Close' onPress={() => setShow(false)} />
-        </View>
-        </View>
-        :null
+  const getAPIData = async () => {  // async for line by line Execution
+    try {
+      const url = "http://192.168.1.5:3000/posts";
+      let result = await fetch(url);
+      let json = await result.json(); //  fix
+      setData(json);                  //  store data
+      setLoading(false);
+    } catch (error) {
+      console.log("Error:", error);
     }
-    <Button title='Open Dialog' onPress={() => setShow(true)} />
-        </View>
-);
+  };
+
+  useEffect(() => {
+    getAPIData();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>FlatList With API</Text>
+
+      {loading ? (
+        <ActivityIndicator size="large" />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.card}>
+              <Text style={styles.text}>{item.title}</Text>
+            </View>
+          )}
+        />
+      )}
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-    justifyContent: "flex-end",
-  },
-  Modal:{
-    flex: 1,
-    backgroundColor: 'rgba(152, 158, 154, 0.5)',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-})
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 40,
+    padding: 10,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  card: {
+    backgroundColor: "#eee",
+    padding: 15,
+    marginVertical: 5,
+    borderRadius: 10,
+  },
+  text: {
+    fontSize: 18,
+  },
+});
+
+
 
 
 
